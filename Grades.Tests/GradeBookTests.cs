ffeaf10cc36d0c0d12684a5f5d78444ceba5c70c
Daniels <Daniels.James@mayo.edu>
2017-02-13 18:41:24 -0600

    Initial commit, kind of

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComutesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.AreEqual(90, result.HighestGrade);
+        }
+
+        [TestMethod]
+        public void ComutesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.AreEqual(10, result.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComutesAvgGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            book.AddGrade(50);
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.AreEqual(50, result.AverageGrade);
+        }
+    }
+}
