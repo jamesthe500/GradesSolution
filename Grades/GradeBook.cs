bfa1bce6f1d6cf3f1b8c374aefa730c7645a1bfb
Daniels <Daniels.James@mayo.edu>
2017-02-18 09:12:03 -0600

    Writes to a file and uses finally to close out the stream. Tightens up main method. m6c8

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -76,7 +76,7 @@ namespace Grades
                     throw new ArgumentException("Name cannot be blank.");
                 }
 
-                if (_name != value) // A delegate to let the program know when the name is changed. Databinding e.g.
+                if (_name != value && NameChanged != null) // A delegate to let the program know when the name is changed. Databinding e.g.
                 {
                     //Name Changed(_name, value) // pseudo-code to guide what delegate to make
                     NameChangedEventArgs args = new NameChangedEventArgs();
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Speech.Synthesis;
 using System.Text;
@@ -11,20 +12,60 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-           
-            GradeBook book = new GradeBook(); //constructor, new kw is invoking a new instance of GradeBook
-           
 
+            GradeBook book = new GradeBook(); //constructor, new kw is invoking a new instance of GradeBook
+
+            // These methods were created by highlighting code and ctrl-. to extract method. Too many things on Main method.
+            GetBookName(book); 
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResults("Average", stats.AverageGrade);
+            WriteResults("Highest", stats.HighestGrade);
+            WriteResults("Lowest", stats.LowestGrade);
+            WriteResults(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            /*
+            StreamWriter outputFile = File.CreateText("grades.txt"); // had to add system.IO namespace to top. Typed "File" ctrl-. to let VS make it easy.
+            book.WriteGrades(outputFile);
+            outputFile.Close(); // necessary to close the stream otherwise the stream gets buffered and nothing sometimes gets written.
+            */
+
+            // This is a better way:
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+                // .Close() isn't needed as the using block does it aoutomatically. StreamWriter has to have that method or dispose() in order for this to work (it does)
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f); //The f instructs to make this a float.
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
             try // b/c the written value could be null and thus throw and exception, we try it first
             {
                 Console.WriteLine("Enter a grade book name.");
                 book.Name = Console.ReadLine();
             }
-            catch(ArgumentException ex) // the ex is a var to hold the exception object thrown.
+            catch (ArgumentException ex) // the ex is a var to hold the exception object thrown.
             {
                 Console.WriteLine(ex.Message); // accessing the Message property of the exception. There are many others like stack trace.
-            } 
-            catch(NullReferenceException) // this exception is happening when no one is subscribed to the NameChanged event.
+            }
+            catch (NullReferenceException) // this exception is happening when no one is subscribed to the NameChanged event.
             {
                 Console.WriteLine("Something went wrong");
             }
@@ -32,17 +73,6 @@ namespace Grades
             {
                 Console.WriteLine("AACK!");
             }
-
-            book.AddGrade(91);
-            book.AddGrade(89.5f); //The f instructs to make this a float.
-            book.AddGrade(75);
-            book.WriteGrades(Console.Out);
-
-            GradeStatistics stats = book.ComputeStatistics();
-            WriteResults("Average", stats.AverageGrade);
-            WriteResults("Highest", stats.HighestGrade);
-            WriteResults("Lowest", stats.LowestGrade);
-            WriteResults(stats.Description, stats.LetterGrade);
         }
 
         static void WriteResults(string description, float result)
